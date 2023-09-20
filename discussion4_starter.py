c33823ab7ebb2498fe7a42dd14cd6ec23a0782c3
Jacob Ellenbogen <jellenbo@umich.edu>
2023-09-20 17:17:59 -0400

    string method

--- discussion4_starter.py
+++ discussion4_starter.py
@@ -17,6 +17,8 @@ class Rectangle():
     #       "A rectangle with width ____ and height ____"
 
     # YOUR CODE HERE
+    def __str__(self):
+        return "A rectangle with width " + str(self.width) + " and height " + str(self.height)
 
 
 
